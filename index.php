096d21948247472abb67229cb2216e5b092f4a62
Luke Howard <luke@lukehoward.me.uk>
2013-08-19 16:12:42 +0100

    Begin
    Added Mooi framework to folder

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- ======== Basic Page Needs ========= -->    
+<meta charset="utf-8">
+<title>Mooi | A Beautifully Clean and Simple SCSS Framework</title>
+
+<!-- ======== Responsive Page Needs ========= -->    
+    
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- ======== Mooi Layout, and Responsive Needs ========= -->
+    
+<!-- HTML5Shiv.js & Respond.js -->
+<!--[if lte IE 8]>
+<script type="text/javscript" src="jquery/html5shiv.js"></script>
+<script type="text/javscript" src="jquery/respond.js"></script>
+<![endif]-->
+
+<!-- ======== CSS StyleSheets ========= -->    
+    
+<link href="stylesheets/style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+<!-- ======== Mooi Layout, Delete everything below and create your own site! :-) ========= -->
+
+<header class="row">
+    <div class="sixteen columns">
+        <h2>Mooi Framework</h2>
+    </div>    
+    
+</header>
+
+<div class="row">
+    
+    <div class="one-third column">
+        <p class="intro">Mooi is a SCSS Framework designed to speed up development times, with Mooi you can wireframe a site in a matter of minutes by setting a few variables and then adding your own CSS to the StyleSheets.</p>
+    </div>
+        
+    <div class="two-thirds column">
+    
+    <p class="intro">Mooi has been designed, developed and created with users in mind, the concept is that nothing should be overly complicated and that by using this framework it will reduce the lines of code that you have to write, and let Mooi do the rest for you.</p>
+    
+    </div>
+    
+    
+</div>    
+    
+<footer class="row">
+    <div class="sixteen columns">
+    	<p>&copy; Copyright 2013. Luke Howard. All Rights Reserved.</p>
+    </div>
+</footer>
+
+</body>
+</html>
